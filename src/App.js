70c7491782197f34acf0f087c434a81a19d559ad
Ashish Kumar Burnwal <ashish293.dev@gmail.com>
2022-07-24 22:01:48 +0530

    project first build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import CategoriesScreen from "./pages/CategoriesScreen";
+import "bootstrap-icons/font/bootstrap-icons.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<>
+			<Header />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/category/:category_name" element={<CategoriesScreen />} />
+
+				<Route path="cart" element={<Cart />} />
+			</Routes>
+		</>
+	);
+};
 
 export default App;
